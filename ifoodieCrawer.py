77ac0e54e6f909b0bd3f573de23cbb0694b0709d
snoopy831002 <snoopy831002@gmail.com>
2016-10-23 20:23:53 +0800

    add discriptions

--- ifoodieCrawer.py
+++ ifoodieCrawer.py
@@ -1,12 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
+'''
+  ifoodieCrawer.py
+  @about-Parse data from weekly ranking from ifoodie according to city 
+  Created by 中皓 李 on 2016/10/18.
+  Copyright © 2016 中皓 李. All rights reserved.
+'''
+import requests 
+from bs4 import BeautifulSoup #@note-(install via pip3)
+from selenium import webdriver #@note-parse ajax call back from website(install via pip3)
 
-requestURL = 'https://ifoodie.tw/ranking/weekly/%E5%8F%B0%E5%8D%97/' 
-driver = webdriver.PhantomJS(executable_path=r'/Users/zhonghaoli/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')  # PhantomJS
+city='台中'
+requestURL = 'https://ifoodie.tw/ranking/weekly/'+city+'/' 
+driver = webdriver.PhantomJS(executable_path=r'/Users/zhonghaoli/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs') # PhantomJS
 driver.get(requestURL)
 pageSource = driver.page_source 
 soup = BeautifulSoup(pageSource, 'html.parser')
 result = soup.findAll("h4",{"class","title"})
 for i in result: 
-     print(i.a['href'])
+     print('https://ifoodie.tw'+i.a['href'])
